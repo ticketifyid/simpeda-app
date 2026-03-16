12448d0ce05a48fd70540bce6482ca0a043d0f60
dwiprakoso <dwiprakoso015@gmail.com>
2026-03-16 07:07:33 +0700

    fix : meta tag auth page

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,33 +1,31 @@
 <!DOCTYPE html>
 <!--
-Author: Keenthemes
-Product Name: MetronicProduct Version: 8.2.5
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
+Author: Ticketify
+Product Name: Simpeda
+Product Version: 1.0.0
+Purchase: https://ticketify.id
+Website: https://ticketify.id
+Contact: support@ticketify.id
+Follow: www.twitter.com/ticketify
+Like: www.facebook.com/ticketify
+License: For each use you must have a valid license purchased from Ticketify.
 -->
 <html lang="en">
 <!--begin::Head-->
 
 <head>
     <base href="{{ url('/') }}/" />
-    <title>Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes</title>
+    <title>Simpeda - Ticketify</title>
     <meta charset="utf-8" />
-    <meta name="description"
-        content="The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords"
-        content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="description" content="Simpeda by Ticketify" />
+    <meta name="keywords" content="simpeda, ticketify" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Metronic by Keenthemes" />
-    <link rel="canonical" href="http://authentication/layouts/corporate/sign-in.html" />
+    <meta property="og:title" content="Simpeda - Ticketify" />
+    <meta property="og:url" content="https://simpeda-dev.ticketify.id" />
+    <meta property="og:site_name" content="Simpeda by Ticketify" />
+    <link rel="canonical" href="https://simpeda-dev.ticketify.id" />
     <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
@@ -37,7 +35,10 @@ License: For each use you must have a valid license purchased only from above li
     <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
     <script>
-        // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }
+        // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
+        if (window.top != window.self) {
+            window.top.location.replace(window.self.location.href);
+        }
     </script>
 </head>
 <!--end::Head-->
@@ -136,7 +137,7 @@ License: For each use you must have a valid license purchased only from above li
                     <!--begin::Text-->
                     <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
                         <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces
-                        a person they’ve interviewed
+                        a person they've interviewed
                         <br />and provides some background information about
                         <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and
                         their
